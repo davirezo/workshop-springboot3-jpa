8563ba9399d55add57df31f5ea8bbae1aa6291d1
davirezo <fksdavi@gmail.com>
2025-12-23 10:55:40 -0300

    OrderItem, many-to-many association with extra attributes

--- src/main/java/com/educandoweb/course/entities/Order.java
+++ src/main/java/com/educandoweb/course/entities/Order.java
@@ -4,10 +4,13 @@ package com.educandoweb.course.entities;
 import com.educandoweb.course.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
+import org.aspectj.weaver.ast.Or;
 
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 @Entity
 @Table(name = "tb_Order" )
@@ -27,6 +30,8 @@ public class Order implements Serializable {
     @JoinColumn(name = "client_id")
     private User client;
 
+    @OneToMany(mappedBy = "id.order")
+    private Set<OrderItem> items = new HashSet<>();
     public Order(){
     }
 
@@ -70,6 +75,9 @@ public class Order implements Serializable {
         this.momennt = momennt;
     }
 
+    public Set<OrderItem> getItems(){
+        return items;
+    }
     @Override
     public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) return false;
