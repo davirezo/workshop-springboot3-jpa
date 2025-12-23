6e487ebdc4d1b98cc36d73889dca90706cadbf09
davirezo <fksdavi@gmail.com>
2025-12-23 11:23:24 -0300

    Product-OrderItem one-to-many association

--- src/main/java/com/educandoweb/course/entities/OrderItem.java
+++ src/main/java/com/educandoweb/course/entities/OrderItem.java
@@ -39,7 +39,7 @@ public class OrderItem implements Serializable {
         id.setOrder(order);
     }
 
-    public Product getOProduct(){
+    public Product getProduct(){
         return id.getProduct();
     }
 
