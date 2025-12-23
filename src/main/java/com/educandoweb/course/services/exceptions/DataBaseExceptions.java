979d2865d6ea80909b0f60e2531d1270b3cbbcff
davirezo <fksdavi@gmail.com>
2025-12-23 13:59:26 -0300

    Exception handling - delete

---
+++ src/main/java/com/educandoweb/course/services/exceptions/DataBaseExceptions.java
@@ -0,0 +1,8 @@
+package com.educandoweb.course.services.exceptions;
+
+public class DataBaseExceptions extends RuntimeException{
+
+    public DataBaseExceptions(String msg){
+        super(msg);
+    }
+}
